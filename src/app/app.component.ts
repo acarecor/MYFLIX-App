3c0b846a5dfea43847b31bf55214668f50a6cb17
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-10-24 11:51:08 +0200

    relocated login and signup logic in welcome component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-//import components
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -12,24 +7,4 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  constructor(public dialog: MatDialog){}
-  //this function open the the dialog when signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '480px'
-    });
-  }
-//this function open the the dialog when login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '480px'
-    });
-  }
-  //this function open the the dialog when allMovies button is clicked
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
-    });
-  }
 }
