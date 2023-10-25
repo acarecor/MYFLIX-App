dd74ab87b8ac842593ad55496eda8c3ea7966e69
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-10-25 15:34:40 +0200

    updated profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,38 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
-
 
 //to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
-  
-  user : any ={};
+export class ProfileComponent implements OnInit {
+  user: any = {};
+
+  @Input()
+  userData = {
+    username: ' ',
+    password: ' ',
+    email: ' ',
+    birthday: '',
+  };
+
   constructor(
     public fetchApiData: UserRegistrationService,
-    
     public snackbar: MatSnackBar,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-  
     this.getUserInfo();
   }
   //fetch movie data from the Api
   getUserInfo(): void {
-    
-    this.fetchApiData.getUser().subscribe((response: any) =>{
+    this.fetchApiData.getUser().subscribe((response: any) => {
       this.user = response;
-      console.log(this.user);
-      return this.user;
+      this.userData.username = this.user.username;
+      this.userData.email = this.user.email;
+      this.userData.birthday = this.user.birthday;
+      console.log(this.user.username);
+      
     });
   }
 }
