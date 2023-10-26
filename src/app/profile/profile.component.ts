461093b912fd2685c270c85c08576806b9945b23
Ana Arellano <117821773+acarecor@users.noreply.github.com>
2023-10-26 18:10:04 +0200

    update profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,44 +1,74 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
+//import { MovieCardComponent } from '../movie-card/movie-card.component';
+//import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { formatDate } from '@angular/common';
 
 //to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserUpdateComponent } from '../user-update/user-update.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
   user: any = {};
+  favoritesMovies: any = '';
+  
 
-  @Input()
-  userData = {
-    username: ' ',
-    password: ' ',
-    email: ' ',
-    birthday: '',
-  };
+  userData = { username: '', password: '', email: '', birthday: '', };
 
   constructor(
     public fetchApiData: UserRegistrationService,
     public snackbar: MatSnackBar,
-    private router: Router
+    private router: Router,
+  
+    public dialog: MatDialog,
+  
   ) { }
 
   ngOnInit(): void {
+  
     this.getUserInfo();
+   
+    
   }
-  //fetch movie data from the Api
+  
   getUserInfo(): void {
     this.fetchApiData.getUser().subscribe((response: any) => {
       this.user = response;
       this.userData.username = this.user.username;
       this.userData.email = this.user.email;
-      this.userData.birthday = this.user.birthday;
-      console.log(this.user.username);
+      this.userData.birthday = formatDate(this.user.birthday, 'dd-MM-yyyy', 'en-US', 'UTC+0');
       
+      this.fetchApiData.getAllMovies().subscribe((response: any) => {
+        this.favoritesMovies = response.filter((m: { _id: any }) => this.user.favoritesMovies.indexOf(m._id) >= 0)
     });
+  })}
+
+    changeInfo(): void {
+      this.dialog.open(UserUpdateComponent, {
+        width: '280px'
+      });
+    }
+
   }
-}
+
+  
+  
+
+
+   
+
+
+
+ 
+
+
+
+
+
