fada56ef9db59904e70287cb47e1df0664e0f842
acarecor <acarellanoc@gmail.com>
2026-02-23 10:21:40 +0100

    chore: update API URL after backend deployment migration

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -6,7 +6,7 @@ import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
 
-const apiUrl = 'https://myflix-movies-2a93844126ef.herokuapp.com/';
+const apiUrl = 'https://movie-api-j617.onrender.com';
 @Injectable({
   providedIn: 'root'
 })
